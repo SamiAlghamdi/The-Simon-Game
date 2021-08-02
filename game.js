f2f83ff3bead162ff777bbe0f258b706d0f8da2d
SamiWAlghamdi <sami10073@gmail.com>
2021-08-03 01:57:24 +0300

    finish game functionality

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-//==============================================================================
 //variables
 var buttonColors = ["green", "red", "yellow", "blue"];
 
@@ -6,45 +5,13 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
-var level = 1;
+var level = 0;
 
-//==============================================================================
-//start the game
-
-$(document).keydown(function(){
-  if (event.keyCode === 13){
-    nextSequence();
-  }
-});
-
-//==============================================================================
-//creating the sequence
-
-function nextSequence() {
-  var randomNumber = Math.random() * 4;
-  randomNumber = Math.floor(randomNumber);
-
-  //choosing a random color
-  randomChosenColor = buttonColors[randomNumber];
-
-  //adding the the chosen color to an array ("gamePattern")
-  gamePattern.push(randomChosenColor);
-
-  $("h1").text("Level " + level++);
-
-
-}
-
-//==============================================================================
-//flashing animation
-
-// $("#" + randomChosenColor).fadeOut(250).fadeIn(250);
+var started = false;
 
 //==============================================================================
 
-
-$(".btn").on("click",function(){
-  //playing audio when a button is clicked
+$(".btn").click(function() {
   var audio = new Audio("sounds/click.mp3");
   audio.volume = 0.3;
   audio.play();
@@ -54,11 +21,79 @@ $(".btn").on("click",function(){
   userClickedPattern.push(userChosenColor);
 
   //adding animation when a button is pressed
-  var aa= event.target;
+  var aa = event.target;
   $(aa).addClass("pressed");
 
-  setTimeout(function(event){ $(aa).removeClass("pressed"); }, 100);
+  setTimeout(function(event) {
+    $(aa).removeClass("pressed");
+  }, 100);
 
+  checkAnswer(userClickedPattern.length-1);
 
 
 });
+
+
+//==============================================================================
+
+$(document).keydown(function() {
+  if (!started) {
+    if (event.keyCode === 13) {
+      $("h1").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  }
+
+});
+
+//==============================================================================
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.random() * 4;
+  randomNumber = Math.floor(randomNumber);
+
+  randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(250).fadeIn(250);
+
+}
+
+//==============================================================================
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1200);
+    }
+  } else {
+    $("h1").text("Game Over, Press Enter to Restart");
+
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.volume = 0.03;
+    audio.play();
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    level = 0;
+    started = false;
+    gamePattern = [];
+
+  }
+
+}
